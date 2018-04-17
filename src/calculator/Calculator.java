e6db79aaddf44d3cf043602977c49c7aeb7c164e
abensour <37442472+abensour@users.noreply.github.com>
2018-04-17 14:30:59 +0300

    //

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,8 +6,14 @@ public class Calculator {
 		Scanner sc = new Scanner(System.in);
 		boolean toExit = false;
 		System.out.println("Please select the scalar field" );
-		String sr = sc.nextLine();
-		
+		String field = sc.nextLine();
+		if(field == "R")
+			isReal = true;
+		else
+			isReal = false;
+		while(!toExit){
+			
+		}
 	
 	}
 
